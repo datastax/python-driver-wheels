6cf96674477e30b596c21200d4126878bdd7d63a
Bret McGuire <bret.mcguire@datastax.com>
2021-09-02 15:43:19 -0500

    Let's maybe not use paths from Docker images

--- build_wheel.sh
+++ build_wheel.sh
@@ -10,5 +10,5 @@ clean_code $REPO_DIR $BUILD_COMMIT
 build_wheel $REPO_DIR $PLAT
 install_run $PLAT
 
-cd /io/wheelhouse
+cd wheelhouse
 curl -u $ARTIFACTORY_USER:$ARTIFACTORY_PASS -T "{$(echo cassandra_driver* | tr ' ' ',')}" "https://datastax.jfrog.io/datastax/python-drivers/python-driver-wheels/builds/{$TRAVIS_BUILD_NUMBER}/"
